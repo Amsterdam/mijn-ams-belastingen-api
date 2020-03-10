c9603d5e2465a9c5928b358f4baacda2de4578ec
Johan Otten <drakonen@drakonen.com>
2020-03-10 15:20:03 +0100

    Translate bsns for acc

--- belastingen/tests/test_config.py
+++ belastingen/tests/test_config.py
@@ -3,7 +3,9 @@ from unittest import TestCase
 from unittest.mock import patch
 
 
-from belastingen.config import get_sentry_dsn, get_tma_certificate, get_K2B_api_location, get_bearer_token
+from belastingen.config import get_sentry_dsn, get_tma_certificate, get_K2B_api_location, get_bearer_token, \
+    get_bsn_translations
+from belastingen.tests import FIXTURE_PATH
 
 
 class ConfigTest(TestCase):
@@ -28,3 +30,13 @@ class ConfigTest(TestCase):
     def test_get_bearer_token(self):
         value = get_bearer_token()
         self.assertEqual(value, 'k2bbearertoken')
+
+    @patch.dict(os.environ, {'BSN_TRANSLATIONS_FILE': f'{FIXTURE_PATH}/bsn_translations_empty.json'})
+    def test_get_bsn_translations(self):
+        value = get_bsn_translations()
+        self.assertEqual(value, {})
+
+    @patch.dict(os.environ, {'BSN_TRANSLATIONS_FILE': f'{FIXTURE_PATH}/bsn_translations.json'})
+    def test_get_bsn_translations(self):
+        value = get_bsn_translations()
+        self.assertEqual(value, {'123': '234', '345': '456'})
